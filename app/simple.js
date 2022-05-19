ae55df6a3bc4a8568e094251c86c5d8e67f02a77
Louay122 <Louay.gourrida@esprit.tn>
2022-05-19 15:13:54 +0100

    fix webhook

--- app/simple.js
+++ app/simple.js
@@ -16,7 +16,7 @@ function handler(req, res){
     // The Express.js web framework helpfully does just that
     // For simplicity's sake we will always respond with 'hello world' here
     // var hello = req.body.hello;
-    var hello = 'World!';
+    var hello = 'World';
     res.setHeader('Content-Type', 'text/html');
     res.writeHead(200);
     res.end("<html><body><h1>Hello "+hello+"</h1></body></html>");
